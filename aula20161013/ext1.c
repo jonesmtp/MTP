4ea220f7e86ff2aa8bfd3d867411cb0333340bfc
jonesmtp <joaovictorabaete@hotmail.com>
2016-12-05 17:42:34 -0200

    Fim de aula

--- aula20161013/ext1.c
+++ aula20161013/ext1.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main(){
-    float * vetor = NULL;
-    int opc;
-
-    do{
-        vetor = recebeNumero(vetor,...);
-        printf(...,media());
-        printf(...,maior());
-        printf(...,menor());
-        printf("Outro? (0 - nao, 1 - sim):");
-        scanf(%d, &opc);
-    }while(opc !=0);
-    return 0;
+int main()
+{
+	double *vetor= NULL, numero, soma=0, maior=-999999, menor=9999999999;
+	int i, tamanho=0, opcao;
+	do
+	{
+	printf("Digite um numero:\n");
+	scanf("%lf", &numero);
+	if (vetor==NULL)
+		vetor=(double*)malloc(sizeof(double));
+	else
+		vetor=(double*)realloc(vetor, sizeof(vetor)+8); //	vetor=(double*)realloc(vetor, tamanho*sizeof(double)+sizeof(double)) ou vetor=realloc(vetor, (1+tamanho)*sizeof(double))		
+	vetor[tamanho]=numero;
+	tamanho++;
+	soma=soma+numero;
+	if(numero<menor)
+		menor=numero;
+	if(numero>maior)
+		maior=numero;
+	printf ("O menor valor e' %g e o maior e' %g\n", menor, maior);
+	printf("A media e' %g \n", soma/tamanho);
+	printf("Para sair digite 0 e para continuar aperte qualquer tecla diferente de zero.\n");
+	scanf("%d", &opcao);
+}while(opcao!=0);
+free(vetor);
+return 0;
 }
-    float * recebeNumero(float * vetor,...){
-        if(vetor == NULL)...vetor = ...malloc...
-        else...vetor = ...realloc...
-        return vetor;
-    }
-
